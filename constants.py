93ea360e1f221abe58200adf26a0ba7cfba46799
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2025-03-03 16:29:11 -0800

    initial commit

---
+++ constants.py
@@ -0,0 +1,8 @@
+
+
+
+# Constants
+OPENWEATHERMAP = "http://api.openweathermap.org/geo/1.0"
+ZIP_ENDPOINT = OPENWEATHERMAP + "/zip?"
+CITY_STATE_ENDPOINT = OPENWEATHERMAP + "/direct?"
+ZIPCODE_LOOKUP = "api.zippopotam.us/us/"
